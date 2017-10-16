6222bc0eb2d83e961a4f608757c250471cd1f58b
Charlie Brown <cbrown@brock.ac.uk>
2017-10-16 11:48:20 +0100

    Add project files.

---
+++ graphsBFS/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphsBFS
+{
+    class Program
+    {
+        class aNode
+        {
+            private List<int> _adjList = new List<int>();
+            public List<int> adjList { get { return _adjList; } set { _adjList = value; } }
+            public void addNeighbour(int neighbour)
+            {
+                _adjList.Add(neighbour);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            aNode[] graph = new aNode[100];
+            bool[] Discovered = new bool[100];
+            bool[] CompletelyExplored = new bool[100];
+            int numberOFNodes = 7; // this is + 1 as being 1 based
+            //int S = 1, D = 6;
+            int S = 1, D = 5;
+            for (int i = 0; i < numberOFNodes; i++)
+            {
+                graph[i] = new aNode();
+            }
+            makeUpGraph(ref graph);
+            findShortestRoute(S, D, graph, numberOFNodes );
+            Console.ReadLine();
+        }
+
+        private static void findShortestRoute(int S, int D, aNode[] graph, int numberOFNodes)
+        {
+            Queue<int> theQueue = new Queue<int>();
+            bool[] Discovered = new bool[numberOFNodes];
+            int[] Parent = new int[numberOFNodes];
+            int V, C;
+            bool Found = false;
+            theQueue.Enqueue(S);
+            Discovered[S] = true;
+            while (theQueue.Count != 0 && Found == false)
+            {
+                V = theQueue.Dequeue();
+                foreach (var U in graph[V].adjList)
+                {
+                    if (Discovered[U] == false && Found == false)
+                    {
+                        theQueue.Enqueue(U);
+                        Discovered[U] = true;
+                        Parent[U] = V;
+                        if (U == D)
+                        {
+                            Found = true;
+                        }
+                    }
+                }
+            }
+            if (Found == true)
+            {
+                C = D;
+                Console.WriteLine(D); 
+                do
+                {
+                    C = Parent[C];
+                    Console.WriteLine(C);
+                } while (C != S);
+            }
+        }
+
+        private static void makeUpGraph(ref aNode[] graph)
+        {
+            //graph[1].addNeighbour(2);
+            //graph[1].addNeighbour(4);
+            //graph[2].addNeighbour(1);
+            //graph[2].addNeighbour(3);
+            //graph[2].addNeighbour(5);
+            //graph[3].addNeighbour(2);
+            //graph[3].addNeighbour(5);
+            //graph[3].addNeighbour(6);
+            //graph[4].addNeighbour(1);
+            //graph[4].addNeighbour(5);
+            //graph[5].addNeighbour(2);
+            //graph[5].addNeighbour(4);
+            //graph[5].addNeighbour(3);
+            //graph[6].addNeighbour(3); 
+            //extension ex
+            graph[1].addNeighbour(2);
+            graph[1].addNeighbour(3);
+            graph[2].addNeighbour(1);
+            graph[2].addNeighbour(3);
+            graph[2].addNeighbour(4);
+            graph[3].addNeighbour(1);
+            graph[3].addNeighbour(2);
+            graph[3].addNeighbour(5);
+            graph[4].addNeighbour(2);
+            graph[4].addNeighbour(5);
+            graph[4].addNeighbour(6);
+            graph[5].addNeighbour(4);
+            graph[5].addNeighbour(3);
+            graph[6].addNeighbour(4);
+
+
+        }
+
+
+
+    }
+}
